05ef9c7fddd23c83277a9747b7b73471c3bee329
Austin Parvin <austin.parvin@gmail.com>
2020-05-06 08:31:08 -0400

    "Try using the DateTime's built-in AddSeconds method to simplify the code. Note that AddSeconds returns a new DateTime instance and does not modify the instance it is called on."

--- csharp/gigasecond/Gigasecond.cs
+++ csharp/gigasecond/Gigasecond.cs
@@ -4,10 +4,6 @@ public static class Gigasecond
 {
     public static DateTime Add(DateTime moment)
     {
-        // Use a TimeSpan to Add() a duration to a datetime
-        // If given in seconds use FromSeconds()
-
-        var interval = TimeSpan.FromSeconds(1000000000);
-        return moment.Add(interval);
+        return moment.AddSeconds(1000000000);
     }
 }
