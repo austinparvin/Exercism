e64d16e414a6374c4f370945246e5b1898936be9
Austin Parvin <austin.parvin@gmail.com>
2020-05-18 09:28:51 -0400

    logic implemented, error encountered

---
+++ csharp/pangram/Pangram.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+public static class Pangram
+{
+    public static bool IsPangram(string input)
+    {
+        // Remove spaces and convert to all uppercase
+        // the quick brown fox jumps over the lazy dog
+        // THEQUICKBROWNFOXJUMPSOVERTHELAZYDOG
+        input.Replace(" ", "").ToUpper();
+
+        // Create empty boolean array to hold marked values
+        bool[] a = new bool[26];
+
+        var i = 0;
+        while (i < input.Length)
+        {
+            var index = input[i] - 65;
+            a[index] = true;
+            i++;
+        }
+
+        foreach (var item in a)
+        {
+            System.Console.WriteLine(item);
+        }
+        return a.All(x => x);
+    }
+
+}
