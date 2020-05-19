02abcec2425599e0e55bc4019dc0e22429e1cab8
Austin Parvin <austin.parvin@gmail.com>
2020-05-19 11:46:32 -0400

    update

--- csharp/resistor-color/ResistorColor.cs
+++ csharp/resistor-color/ResistorColor.cs
@@ -2,29 +2,14 @@
 
 public static class ResistorColor
 {
+    public static string[] colors = { "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white" };
     public static int ColorCode(string color)
     {
-        // Dictionary<string, int> colors = new Dictionary<string, int>();
-        // colors.Add("black", 0);
-        // colors.Add("brown", 1);
-        // colors.Add("red", 2);
-        // colors.Add("orange", 3);
-        // colors.Add("yellow", 4);
-        // colors.Add("green", 5);
-        // colors.Add("blue", 6);
-        // colors.Add("violet", 7);
-        // colors.Add("grey", 8);
-        // colors.Add("white", 9);
-        // return colors[color.ToLower()];
-
-        var colors = new[] { "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white" };
-
         return Array.IndexOf(colors, color);
     }
 
     public static string[] Colors()
     {
-        var colors = new[] { "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white" };
         return colors;
     }
 }
